99b39adbe17291f6f4cae5bfe0b17bf216df505e
navneet2723 <124881760+navneet2723@users.noreply.github.com>
2025-02-19 17:51:04 +0530

    Sheet-Array-Chocolate Distribution Problem

---
+++ Arrays/ChocolateDistributionProblelm/BFA.java
@@ -0,0 +1,38 @@
+/*
+  time complexity--O(n)
+  space complexity -- O(1)
+ */
+import java.util.*;
+public class BFA {
+    public static int findMinDiff(int[] arr, int m){
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        
+       int i =0;
+       int j = i+m-1;
+        
+        while(j<arr.length){
+            int trackDiff = arr[j]-arr[i];
+            if(min > trackDiff){
+                min = trackDiff;
+            }
+            i++;
+            j++;
+        }
+        return min;
+    }
+    public static void main(String[] args){
+        int[] arr = {3, 4, 1, 9, 56, 7, 9, 12};
+        int m =5;
+        int minDiff = findMinDiff(arr,m);
+        System.out.println(minDiff);
+
+        int[] arr2 = {7, 3, 2, 4, 9, 12, 56};
+        int m2 = 3;
+        System.out.println(findMinDiff(arr2, m2));
+
+        int[] arr3 = {3, 4, 1, 9, 56};
+        int m3 = 5;
+        System.out.println(findMinDiff(arr3,m3));   
+    }
+}
---
+++ Arrays/ChocolateDistributionProblelm/UsingArrayList.java
@@ -0,0 +1,36 @@
+/*
+  time complexity--O(n)
+  space complexity -- O(1)
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class UsingArrayList {
+    public static  int findMinDiff(ArrayList<Integer> arr, int m) {
+        int n  = arr.size();
+        
+        if(n<m){
+            return -1;
+        }
+        Collections.sort(arr);
+        int minDiff = Integer.MAX_VALUE;
+        
+        for(int i = 0; i+m-1 < n; i++){
+            int diff = arr.get(i+m-1) - arr.get(i);
+            
+            minDiff = Math.min(minDiff,diff);
+        }
+        return minDiff;
+    }
+    public static void main(String[] args) {
+        // Directly initializing the ArrayList using Arrays.asList()
+        ArrayList<Integer> chocolates = new ArrayList<>(Arrays.asList(12, 4, 7, 9, 2, 23, 25, 41, 40, 40, 48, 59));
+        
+        int m = 5;  
+        
+        int result = findMinDiff(chocolates, m);
+        System.out.println("The minimum difference is: " + result);
+    }
+}
