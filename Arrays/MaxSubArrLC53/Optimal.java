b857f9395f7c37df948e64a50b11116c9132e9fe
navneet2723 <124881760+navneet2723@users.noreply.github.com>
2025-02-19 15:22:00 +0530

    Sheet-Array-MaxSubArrSum_LC53 change in some line

--- Arrays/MaxSubArrLC53/Optimal.java
+++ Arrays/MaxSubArrLC53/Optimal.java
@@ -9,15 +9,16 @@ public class Optimal {
     public static int kadaneAlgo(int[] arr){
         int maxSum = Integer.MIN_VALUE;
         int currentSum = 0;
-        for(int i = 0; i < arr.length-1; i++){
+        for(int i = 0; i < arr.length; i++){
             currentSum+=arr[i];
+            maxSum = Math.max(maxSum,currentSum);
             if(currentSum<0){
                 currentSum = 0;
             }
             // if(maxSum<currentSum){
             //     maxSum = currentSum;
             // }
-            maxSum = Math.max(maxSum,currentSum);
+            
         }
         return maxSum;
     }
